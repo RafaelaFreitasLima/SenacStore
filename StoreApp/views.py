f602954ea0e0719a0bfc2aa71ca2578cea75ee80
Rafaela Lima <rafaelafreitaslima333@gmail.com>
2023-12-04 22:28:54 -0300

    atualizacoes0412

--- StoreApp/views.py
+++ StoreApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from StoreApp.models import Departamento
 from StoreApp.models import Produto
+from StoreApp.forms import ContatoForm
 
 # Create your views here.
 
@@ -12,6 +13,7 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
+
 
 def produto_lista(request):
     # buscando produtos no banco 
@@ -40,10 +42,23 @@ def produto_lista_por_id(request, id):
 
 def produto_detalhe(request, id):
     produto = Produto.objects.get(id = id)
+    produtos_relacionados = Produto.objects.filter(departamento_id = produto.departamento.id).exclude(id = id)[:4]
+
 
     context = {
-        'produto' : produto
+        'produto' : produto,
+        'produtos_relacionados' : produtos_relacionados
     }
     return render(request, 'produto_detalhes.html', context)
 
+def institucional(request):
+    return render(request, 'institucional.html')
 
+def contato(request):
+    formulario = ContatoForm()
+
+    context = {
+        'form_contato' : formulario
+    }    
+    
+    return render(request, 'contato.html', context)
